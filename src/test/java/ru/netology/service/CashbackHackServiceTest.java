051414d6cbd85a8442072dfd99d2080304348456
ArtemiiSi <107568989+ArtemiiSi@users.noreply.github.com>
2023-05-31 01:39:22 +0300

    initial commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,40 +1,39 @@
 package ru.netology.service;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
     CashbackHackService cashback = new CashbackHackService();
+    
+    @Test
+    public void shouldAmountEqual1000() {
+        int amount = 1000;
+
+        int expected = 0;
+        int actual = cashback.remain(amount);
+
+        assertEquals(actual, expected);
+    }
 
     @Test
+    public void shouldAmountAbove1000() {
+        int amount = 1200;
 
-        public void shouldAmountEqual1000() {
-            int amount = 1000;
+        int expected = 800;
+        int actual = cashback.remain(amount);
 
-            int expected = 0;
-            int actual = cashback.remain(amount);
+        assertEquals(actual, expected);
+    }
 
-            assertEquals(expected, actual);
-        }
+    @Test
+    public void shouldAmountBelow1000() {
+        int amount = 800;
 
-        @Test
-        public void shouldAmountAbove1000() {
-            int amount = 1200;
+        int expected = 200;
+        int actual = cashback.remain(amount);
 
-            int expected = 800;
-            int actual = cashback.remain(amount);
-
-            assertEquals(expected, actual);
-        }
-
-        @Test
-        public void shouldAmountBelow1000() {
-            int amount = 800;
-
-            int expected = 200;
-            int actual = cashback.remain(amount);
-
-            assertEquals(expected, actual);
-        }
+        assertEquals(actual, expected);
+    }
 }
